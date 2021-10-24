c0b5e338501c622259fa488aa632570fe25ae669
jonas <sentinaano@web.de>
2021-10-24 21:17:47 +0200

    proper ranges

--- Project2/parameters.py
+++ Project2/parameters.py
@@ -2,13 +2,15 @@ import numpy as np
 
 # eta, beta1, beta2
 
-eta_range = np.logspace(0.0001, 0.1, 500)
-beta1_range = np.logspace(0.1, 0.999, 500)
-beta2_range = np.logspace(0.1, 0.9999, 500)
+eta_range = np.logspace(np.log10(0.0001), np.log10(0.1), 500)
+beta1_range = np.logspace(np.log10(0.1), np.log10(0.999), 500)
+beta2_range = np.logspace(np.log10(0.1), np.log10(0.9999), 500)
 
 etas = np.random.choice(eta_range, size=10)
 beta1s = np.random.choice(beta1_range, size=10)
 beta2s = np.random.choice(beta2_range, size=10)
+
+print(etas)
 
 
 def write_config_files(etas, beta1s, beta2s, folder):
@@ -25,7 +27,7 @@ def write_config_files(etas, beta1s, beta2s, folder):
             f.write(f"batchsize = 5000\n")
             f.write(f"loss_function = MAE")
 
-        with open(folder + "job" + str(i) + '.txt', 'w') as f:
+        with open(folder + "job" + str(i) + '.sh', 'w') as f:
             text = f"""#!/usr/local_rwth/bin/zsh
 #SBATCH --output job{i:}.out
 #SBATCH --time=04:00:00
